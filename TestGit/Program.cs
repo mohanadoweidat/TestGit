08082780a3af67dbd4a26d50f23fa46ec55d4207
Sara <sara.fakhro@outlook.com>
2021-03-15 15:26:46 +0100

    Skrev hej

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -7,6 +7,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hej från sara");
           
         }
     }
