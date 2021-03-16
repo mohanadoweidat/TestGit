72d3f71fd8ae8209bee626c43bf7612ce0d2517b
Justine Nankya Jensen <aj4698@mau.se>
2021-03-16 12:35:08 +0100

    Testing

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -10,7 +10,7 @@ namespace TestGit
             Console.WriteLine("hej från Justine");
             Console.WriteLine("Hejjj Bojana you mastermind");
             Console.WriteLine("Hej från master");
- 
+            Console.WriteLine("Testing, från Justine");
         }
 
 
