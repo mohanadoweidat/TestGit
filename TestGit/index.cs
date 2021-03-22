cc42fbae278957c5e8709af8b7b8ab548b9c4d61
mohanad oweidat <mohanad.owidat@hotmail.com>
2021-03-22 15:50:42 +0100

    Jag har skapat index klassen

---
+++ TestGit/index.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGit
+{
+    class index
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej från index klass");
+            Console.WriteLine("Jag heter mohanad och jag blir utnyttjad!");
+            Console.WriteLine("Vi har förlorat dig min bror!");
+            Console.WriteLine("Inshallah omrra o steen zamanaaa!");
+            Console.WriteLine("Skapad av Mohanad!");
+        }
+    }
+}
