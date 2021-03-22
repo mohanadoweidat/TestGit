fd51f2c37bab08b05501569e034550cdd8609feb
Sarkis Baghdasarian <al2260@mau.se>
2021-03-22 15:51:24 +0100

    jag har fixat

---
+++ TestGit/signUp-class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGit
+{
+    class signUp_class
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hej från bananna ");
+            Console.WriteLine("bananna bananna ");
+            Console.WriteLine("qozolkort");
+
+        }
+    }
+}
